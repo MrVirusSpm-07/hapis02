f879665f70e3c7df4907de30dd881c6dfd3f7795
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-06-15 21:03:34 +0700

    Add files via upload

---
+++ gasken.sh
@@ -0,0 +1,27 @@
+
+clear
+echo
+sleep 1
+xdg-open https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ
+echo
+figlet HACK-FB
+echo
+    echo "AUTHOR : MR VIRUS SPM"
+    echo "TEAM   : MAFIA TEKNOLOGI"
+    echo "KONTAK : +62 882-1235-2864"
+echo
+echo "MASUKAN NAMA ANDA"
+read -p "=> " nama;
+sleep 1
+echo
+echo " SELAMAT DATANG $nama SELAMAT MENGGUNAKAN TOOL SAYA"
+echo
+sleep 1
+     git clone https://github.com/mrvirusrecord/apaan
+     sleep 1
+     clear
+     cd apaan
+     sleep 1
+     clear
+     php AhJaDiSaYaNgDeH.php
+fi
